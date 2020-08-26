9b9afd31128ad7abd938d4eb79668a4440c6513b
Blakemcw <blakecmcwilliam@gmail.com>
2020-08-25 17:42:55 -0700

    Add finished mock-up for message section of posts

--- src/Components/Messages/Message.js
+++ src/Components/Messages/Message.js
@@ -1,37 +1,89 @@
 // =============================================================================
 // About: Message.js
 // =============================================================================
-/*
+/**
   1) Message contains <Message/>, a component that:
-    - ...
+    - displays the content of a message sent from one user to another.
 
     <Message/>'s Props:
-      - n/a
+      @param {string} sender The username of the person sending the message
+      @param {string} datatimeSent The time the message was sent at
+      @param {string} text The content of the message
 
     <Message/>'s Children:
       - n/a
 
     <Message/>'s Parents:
-      - n/a
+      - <MessageList/>
 */
 
 // Imports ---------------------------------------------------------------------
 
 import React from 'react';
 import './Message.css';
+import ProfilePicture from '../User/ProfilePicture.js';
 
+// Constants -------------------------------------------------------------------
+
+const CURR_USER = 'username';
 
 // =============================================================================
 // <Message/>
 // =============================================================================
 
 class Message extends React.Component {
+
+  format = (this.props.sender === CURR_USER)
+    ? ' sent ' 
+    : ' recieved ';
+
+  renderMessageBox = () => {
+    return (
+      <div class={'message-content' + this.format}>
+        <div class={'message-box' + this.format}>
+          {this.props.text}
+        </div>
+        <div class={'timestamp' + this.format}>
+          Sent at {this.props.datetimeSent} by {this.props.sender}
+        </div>
+      </div>
+    )
+  }
+
+  renderSentMessage = () => {
+    /**
+     * Displays a message with all elements right aligned.
+     */
+    return (
+      <>
+        {this.renderMessageBox()}
+        <ProfilePicture src='https://i.imgur.com/c1Tn08T.jpg'/>
+      </>
+    )
+  }
+
+  renderRecievedMessage = () => {
+    /**
+     * Displays a message with all elements left aligned.
+     */
+    return (
+      <>
+        <ProfilePicture src='https://i.imgur.com/c1Tn08T.jpg'/>
+        {this.renderMessageBox()}
+      </>
+    )
+  }
+
   render() {
     return (
-      <div id="Message">
-        <p>Component: Message</p>
+      <div class="message">
+        {
+          (this.props.sender === CURR_USER)
+            ? this.renderSentMessage()
+            : this.renderRecievedMessage()
+        }
       </div>
-    );
+    )
   }
 }
 
--- src/Components/Messages/MessageList.js
+++ src/Components/Messages/MessageList.js
@@ -3,13 +3,13 @@
 // =============================================================================
 /*
   1) MessageList contains <MessageList/>, a component that:
-    - ...
+    - holds a list of messages for a post
 
     <MessageList/>'s Props:
       - n/a
 
     <MessageList/>'s Children:
-      - n/a
+      - <Message/>
 
     <MessageList/>'s Parents:
       - n/a
@@ -21,6 +21,47 @@ import React from 'react';
 import './MessageList.css';
 import Message from './Message.js';
 
+// Constants -------------------------------------------------------------------
+const LOREM = `lorem ipsum dolor sit amet consectetur adipiscing elit Proin
+               metus elit, dapibus a odio et elementum suscipit sem`;
+const USERS = ['username', 'friend'];
+
+let generatePlaceholderMessage = () => {
+  /**
+   * Generates a message to use for testing Message appearance.
+   */
+  let dictionary = LOREM.split(' ');
+  let numberOfWords = Math.floor(Math.random() * 20) + 1
+
+  let message = ''
+  for (let i = 0; i < numberOfWords; ++i) {
+    let randomWord = Math.floor(Math.random() * dictionary.length);
+    message += dictionary[randomWord] + ' '
+  }
+
+  return message;
+}
+
+let MESSAGES = () => {
+  /**
+   * Generates a list of <Message/>s for testing.
+   */
+  let numberOfMessages = Math.floor(Math.random() * 5) + 1;
+  let user = USERS[1];  // first message always sent by friend
+
+  let messages = [];
+  for (let i = 0; i < numberOfMessages; ++i) {
+    messages.push(
+      <Message 
+          sender  = {user} 
+          text    = {generatePlaceholderMessage()}
+      />
+    )
+    user = USERS[Math.floor(Math.random() * 2)];
+  }
+
+  return messages;
+}
 
 // =============================================================================
 // <MessageList/>
@@ -29,9 +70,10 @@ import Message from './Message.js';
 class MessageList extends React.Component {
   render() {
     return (
-      <div id="message-list">
-        <p>Component: MessageList</p>
-        <Message />
+      <div class="message-list">
+        {MESSAGES().map(message => {
+          return message;
+        })}
       </div>
     );
   }
